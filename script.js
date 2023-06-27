3891e1f695e02fbfade936b661b8d187f9b87ece
lenderisfree <alexsanddro@gmail.com>
2023-06-27 22:00:51 +0100

    Update

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active"); //o toggle quer dizer que, se a classe existir, ele remove, se ela não existir, ela coloca. Ou seja, ele alterna.
+  });
+});
+
+console.log("helloooo");
