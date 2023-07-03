e17f6d5988b5cb5f0a1e1a3f84bfc01304ad0891
lenderisfree <alexsanddro@gmail.com>
2023-07-03 17:38:25 +0100

    Update

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const menu = document.querySelector("#mobile-navbar");
     menu.classList.toggle("menu-active"); //o toggle quer dizer que, se a classe existir, ele remove, se ela não existir, ela coloca. Ou seja, ele alterna.
   });
 });
+
+// Open modal
+const modal = document.querySelector("dialog");
+const openModalBtn = document.querySelector(".btn-sendMail");
+const closeModalBtn = document.querySelector(".closeModal");
+
+openModalBtn.addEventListener("click", () => modal.showModal());
+closeModalBtn.addEventListener("click", () => modal.close());
+
+//clean email information
+
+function cleanField() {
+  document.querySelector("#email").value = "";
+}
