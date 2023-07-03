860b32d45f2697f296b87ef71f40e85078328284
lenderisfree <alexsanddro@gmail.com>
2023-07-03 21:12:39 +0100

    update

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ closeModalBtn.addEventListener("click", () => modal.close());
 function cleanField() {
   document.querySelector("#email").value = "";
 }
+
+// Modal cards
+
+const btnMore = document.querySelectorAll(".btn-more");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalImage = document.querySelector(".modal-image");
+const modalTitle = document.querySelector(".modal-title");
+const modalDescription = document.querySelector(".modal-description");
+const modalPrice = document.querySelector(".modal-price");
+const modalClose = document.querySelector(".modal-close");
+
+btnMore.forEach((button) => {
+  button.addEventListener("click", () => {
+    const product = button.closest(".products");
+    const productImage = product.querySelector(".product");
+    const productTitle = product.querySelector(".product-title").textContent;
+    const productDescription = product.querySelector(
+      ".product-description"
+    ).textContent;
+    const productPrice = product.querySelector(".product-price").textContent;
+
+    modalImage.src = productImage.src;
+    modalTitle.textContent = productTitle;
+    modalDescription.textContent = productDescription;
+    modalPrice.textContent = productPrice;
+  });
+});
